67630b689caf8dfeacce174b968d0ba067237728
Adheesh L P <adheesh.lp@teronext.com>
2024-02-13 00:30:22 -0500

    Refactored and renamed all except app services

--- app/src/androidTest/java/com/example/firebaseprojectgroup2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/firebaseprojectgroup2/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.section1_group2_project1", appContext.packageName)
+        assertEquals("com.example.firebaseprojectgroup2", appContext.packageName)
     }
 }
